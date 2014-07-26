ec2bbd1d223ea949401677ef40479a3aa8727d8e
OneEyeBrewing <matt.carroll@me.com>
2014-07-26 21:39:39 +1000

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'chU8HEWjyedQ3OwSPlOA639rQ7KcFsiPlTz1A3fIkaawRIBe', // Unique master Xively API key to be used as a default
 		defaultFeeds	= 1374874100, // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'OneEyeBrewing', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
